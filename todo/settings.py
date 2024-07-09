909383005924d1d2e217c07fe543ddd7d4f29e2d
Bohdan <bodya190505@gmail.com>
2024-07-09 18:45:15 +0200

    with static

--- todo/settings.py
+++ todo/settings.py
@@ -129,7 +129,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'todoapp/static']
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
